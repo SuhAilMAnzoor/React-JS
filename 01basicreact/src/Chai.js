9d3310539cbd620e9b5f4b20c34ec9f6f6e47839
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-05-05 11:09:19 +0500

    make a function or component and import it in App.js

--- 01basicreact/src/Chai.js
+++ 01basicreact/src/Chai.js
@@ -1,7 +1,12 @@
-function chai(){
+function Chai(){
     return(
         <h2>Chai in React</h2>
     )
 }
 
-export default chai;
+export default Chai;
+
+// in react your function or component name should start
+// with First Captial Letter, and also recommened you 
+// to save file with first letter captial also in
+// Like this Chai and file name is also Chai 
