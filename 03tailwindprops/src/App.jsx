14c7f9f4be015249e2f2fb2c62aa8aab5f78aa43
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-05-31 15:36:13 +0500

    Added Resuable Card Component with this project and pass the values through props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,14 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  // let myObj =  {username : "Sohail",
+  //   age : 22
+  // }
+
+  // let newArrary  = [3,4,5]
 
   return (
     <>
-    <div className='p-70'>
-   <h1 className='bg-green-400 text-white p-4 text-center rounded-xl'>Code ke sath chai aur men</h1>
+    
+    <div className='bg-white-500 flex flex-col h-screen items-center justify-center gap-4'>
+   <h1 className='bg-green-400 text-white p-2 text-center rounded-xl'>Code ke sath chai aur men</h1>
+   <Card username="TYPE YOUR NAME" btnText = "Click me"/>
+   <Card username="TYPE YOUR NAME HERE"/>
+
    </div>
     </>
   )
