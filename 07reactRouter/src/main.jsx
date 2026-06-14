ecd3461c525574a3cde304c29ca16880e706431b
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-06-14 21:53:38 +0500

    Intitlizing react router to perform navigation 2nd way defining routes

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -9,44 +9,45 @@ import Contact from './components/Contacts/Contacts.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RouteDirectory/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      },
-            {
-        path:"contact",
-        element:<Contact/>
-      }
-    ]
-  }
-])
+// previous way to define routes
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <RouteDirectory/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//             {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
 
-// // Recommended Way to Intialize Routes
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path='/' element={<RouteDirectory/>}>
-//       <Route path='' element={<Home/>}/>
-//       <Route path='about' element={<About/>}/>
-//       <Route path='contact' element={<Contact/>}/>
-//       <Route path='user/:userid' element={<User/>}/>
-//       <Route
-//        loader={githubInfoLoader}
-//        path='github'
-//       element={<Github/>}
-//       />
+// Recommended Way 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RouteDirectory/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route
+       loader={githubInfoLoader}
+       path='github'
+      element={<Github/>}
+      />
 
-//     </Route>
-//   )
-// )
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
