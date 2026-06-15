8c961e352f47f15a7b3d15a846ff0f545dd4b1d1
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-06-15 16:27:10 +0500

    updated minor changes

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-import RouteDirectory from './RouteDirectory.jsx'
+import Layout from './Layout.jsx'
 import Contact from './components/Contacts/Contacts.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
@@ -13,7 +13,7 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 // const router = createBrowserRouter([
 //   {
 //     path: '/',
-//     element: <RouteDirectory/>,
+//     element: <Layout/>,
 //     children: [
 //       {
 //         path: "",
@@ -34,7 +34,7 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 // Recommended Way 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RouteDirectory/>}>
+    <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
