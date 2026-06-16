db71ed55816b08bc5f6a5b2d1a1c39a13d0714a2
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-06-16 16:14:34 +0500

    hit Api request to github using UseEffect hook and get data of followers and profile pic and bio data

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,19 +1,17 @@
 
 import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
 
 function Github(){
-    const data = useLoaderData()
-  
-    // const [data, setData] = useState([])
-    // useEffect(() => {
-    //     fetch('https://api.github.com/users/SuhAilMAnzoor')
-    //     .then(response => response.json()).then(data => {
-    //         console.log(data);
-    //         setData(data)
-    //     }
-    //     )
-    // }, [])
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/SuhAilMAnzoor')
+        .then(response => response.json()).then(data => {
+            console.log(data);
+            setData(data)
+        }
+        )
+    }, [])
     
     return (
         <div className='text-center m-4 bg-gray-600
@@ -25,8 +23,3 @@ function Github(){
 }
 
 export default Github
-
-export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/suhailmanzoor')
-    return response.json()
-}
