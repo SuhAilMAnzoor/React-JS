1ead4ec9212bdd39b872cb3ed6da4a3e2c7b4696
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-06-16 10:52:31 +0500

    added all routes to the RouteDirectory then import it in main.jsx

--- 07reactRouter/src/RouteDirectory.jsx
+++ 07reactRouter/src/RouteDirectory.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
-function RouteDirectory(){
-    return (
-        <>
-        <Header/>
-        <Outlet/>
-        <Footer/>
-        </>
-    )
-}
+import { createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contacts/Contacts.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
-export default RouteDirectory
+// previous way to define routes
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//             {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// Recommended Way 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route
+       path='github'
+       element={<Github/>}
+       loader={githubInfoLoader}
+      />
+
+    </Route>
+  )
+)
+
+export default router
