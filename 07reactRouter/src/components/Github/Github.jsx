89c71210ff8c63f14c58ed87ce569e6a6cbc14ff
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-06-16 16:21:08 +0500

    More Opitimized way to call api using useLoaderData hook and get data of github, followers, profile picture etc

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,17 +1,19 @@
 
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github(){
-
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/SuhAilMAnzoor')
-        .then(response => response.json()).then(data => {
-            console.log(data);
-            setData(data)
-        }
-        )
-    }, [])
+    const data = useLoaderData()
+  
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/SuhAilMAnzoor')
+    //     .then(response => response.json()).then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     }
+    //     )
+    // }, [])
     
     return (
         <div className='text-center m-4 bg-gray-600
@@ -23,3 +25,8 @@ function Github(){
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/suhailmanzoor')
+    return response.json()
+}
