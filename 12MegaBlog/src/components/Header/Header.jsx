283d2b97ed84fa117f2d12a27b77887d4373ebe7
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-09-10 18:23:43 +0500

    build responsive header navigation with authentication-based links used reduxto for state when user is logged in show him add post or all posts same as for Login and Signup

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -29,35 +29,40 @@ function Header() {
             slug: "/all-posts",
             active: authStatus
         },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus
+        },
     ]
 
     return (
     <header className="py-3 shadow bg-gray-500">
         <Container>
-            <nav className="flex">
-                <div className="mr-4">
-                    <Link to="/">
-                    <Logo width='70px'/>
-                    </Link>
-                </div>
-                <ul className='flex ml-auto'>
-                {navItems.map((item) => 
-                item.active ? (
-                    <li key={item.name}>
-                        <button
-                        className='inline-bock px-6 py2 duration-200 hover:bg-blue-100 rounded-full'
+<nav className="flex items-center">
+    <div className="mr-4">
+        <Link to="/">
+            <Logo width='70px'/>
+        </Link>
+    </div>
+    <ul className='flex ml-auto items-center gap-x-2'>
+        {navItems.map((item) => 
+            item.active ? (
+                <li key={item.name}>
+                    <button
+                        className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
                         onClick={() => navigate(item.slug)}
-                        >{item.name}</button>
-                    </li>
-                ) : null
-                )}
-                {authStatus && (
-                    <li>
-                        <LogoutBtn/>
-                    </li>
-                )}
-                </ul>
-            </nav>
+                    >{item.name}</button>
+                </li>
+            ) : null
+        )}
+        {authStatus && (
+            <li>
+                <LogoutBtn/>
+            </li>
+        )}
+    </ul>
+</nav>
         </Container>
     </header>
     )
