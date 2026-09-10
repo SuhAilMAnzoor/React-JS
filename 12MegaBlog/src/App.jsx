d8c92065ff2a351738fc42e6cc05deb5a6de120f
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2026-09-10 19:46:46 +0500

    App root with session bootstrap and Redux auth sync

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authServices from './appwrite/authServices'
+import {login, logout} from './features/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
-  return (
-    <>
-     <h1>A blog with Appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServices.getCurrentUser().then(
+      (userData) => {
+        if (userData) {
+          dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+  }).finally(() => setLoading(false))
+  }, [])
+
+   return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet />
+          </main>
+          <Footer/>
+      </div>
+    </div>
+   ) : null
 }
 
 export default App
